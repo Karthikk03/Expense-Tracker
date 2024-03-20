1b319ae9752bd044246c688159547bcb8a26c070
karthikk03 <karthikkonamgeri1@gmail.com>
2024-03-20 11:03:48 +0530

    pagination

--- backend/Controllers/reports.js
+++ backend/Controllers/reports.js
@@ -1,18 +1,17 @@
 const User = require('../Models/User');
-const { Op } = require('sequelize');
+const Expense = require('../Models/Expense');
+const { Op, where } = require('sequelize');
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
-const region='ap-southeast-2';
+const region = 'ap-southeast-2';
 
 const s3 = new S3Client({
     credentials: {
         accessKeyId: process.env.IAM_USER_KEY,
         secretAccessKey: process.env.IAM_USER_SECRET
     },
-    region:region
+    region: region
 })
-
-console.log(s3.region);
 
 async function s3Upload(data, filename) {
     const bucket = process.env.BUCKET_NAME;
@@ -21,37 +20,34 @@ async function s3Upload(data, filename) {
         Bucket: bucket,
         Key: filename,
         Body: data,
-        ACL:'public-read'
+        ACL: 'public-read'
     }
     try {
-        const command=new PutObjectCommand(uploadParams);
+        const command = new PutObjectCommand(uploadParams);
 
         const response = await s3.send(command);
         const url = `https://${bucket}.s3.${region}.amazonaws.com/${encodeURIComponent(filename)}`;
-        return {response,url};
+        return { response, url };
     }
     catch (e) {
         console.log(e);
-        return {error:e.message};
+        return { error: e.message };
     }
 }
 
 exports.getReports = async (req, res, next) => {
     try {
-        const { date, month } = req.query;
+        const { date, month, page_no } = req.query;
         const user = await User.findByPk(req.userId);
         if (!user) res.status(404).json({ error: "User not found" });
 
-        if (user.isPremium !== true) return res.status(403).json({ error: "Access to the requested resource is forbidden for non-premium users." });
+        // if (user.isPremium !== true) return res.status(403).json({ error: "Access to the requested resource is forbidden for non-premium users." });
 
-        let expenses;
+        const start = (page_no - 1) * 10 ||0;
+        let whereClause={};
 
         if (date) {
-            expenses = await user.getExpenses({
-                where: {
-                    date
-                }
-            });
+            whereClause.date = date;
         }
         else {
             const [year, monthNumber] = month.split('-').map(Number);
@@ -59,20 +55,35 @@ exports.getReports = async (req, res, next) => {
             // Create a date object for the first day of the next month, then subtract one day
             const lastDate = new Date(year, monthNumber, 0).getDate();
 
-            expenses = await user.getExpenses({
-                where: {
-                    date: {
-                        [Op.gte]: `${month}-01`,
-                        [Op.lte]: `${month}-${lastDate}`
-                    }
-                },
-            });
+            whereClause.date = {
+                [Op.gte]: `${month}-01`,
+                [Op.lte]: `${month}-${lastDate}`
+            }
         }
-        return res.json(expenses);
 
+        const expenses = await user.getExpenses({
+            where: whereClause,
+            offset: start,
+            limit: 10
+        })
+
+        const response={
+            expenses,
+            current:parseInt(page_no)||1
+        }
+        
+        if(!page_no){
+            const count=await user.countExpenses({
+                where:whereClause
+            })
+            response.lastPage=Math.ceil(count/10);
+        }
+
+        return res.json(response);
     }
 
     catch (e) {
+        console.log(e)
         return res.status(500).json({ error: 'Some internal issue' });
     }
 }
@@ -90,7 +101,7 @@ exports.download = async (req, res, next) => {
         const filename = `Expenses_${user.id}/${date}.txt`
         const uploaded = await s3Upload(JSON.stringify(expenses), filename);
 
-        if(uploaded.error)throw new Error('Failed to Upload');
+        if (uploaded.error) throw new Error('Failed to Upload');
         return res.status(200).json(uploaded.url);
 
     }
--- frontend/reports.js
+++ frontend/reports.js
@@ -1,26 +1,32 @@
-const notPremium=document.querySelector('.not-premium');
+const notPremium = document.querySelector('.not-premium');
+let value;
+let lastPage;
 
 document.addEventListener('DOMContentLoaded', async () => {
     const currentDate = new Date();
     const formattedDate = currentDate.toISOString().split('T')[0];
     try {
         const response = await axios.get(`http://localhost:3000/reports/?date=${formattedDate}`, { headers: { 'Authorization': token } });
-       response.data.forEach(item=>{
-        addRow(item);
-       })
+        response.data.expenses.forEach(item => {
+            addRow(item);
+        })
+        value = formattedDate;
+        showPages(response.data);
+        lastPage = response.data.lastPage;
     }
 
-    catch(e){
+    catch (e) {
         console.log(e)
-        if(e.response.status==403){
-            notPremium.style.display='flex';
-            document.querySelector('.main-container').style.display='none';
+        if (e.response.status == 403) {
+            notPremium.style.display = 'flex';
+            document.querySelector('.main-container').style.display = 'none';
         }
     }
 
-    
+
 
 })
+
 
 const type = document.getElementById('type');
 const dateInput = document.querySelector('.date-input');
@@ -31,6 +37,7 @@ const form = document.getElementById('dmy');
 const date = document.getElementById('date');
 const month = document.getElementById('month');
 let selected = date;
+
 
 const token = localStorage.getItem('token');
 
@@ -51,7 +58,7 @@ form.addEventListener('submit', async (e) => {
     e.preventDefault();
     let queryParam;
     if (selected === month) queryParam = `?month=${selected.value}`
-    else queryParam = `?date=${selected.value}`
+    else queryParam = `?date=${selected.value}`;
     const response = await axios.get(`http://localhost:3000/reports/${queryParam}`, { headers: { 'Authorization': token } });
     tableBody.textContent = ``;
     if (type.value === 'date') {
@@ -63,16 +70,20 @@ form.addEventListener('submit', async (e) => {
         reportDesc.textContent = `Monthly Report`;
     }
 
-    if (response.data.length === 0) {
+    if (response.data.expenses.length === 0) {
         img.style.display = 'block';
         icon.style.display = 'none';
         return;
     }
 
     img.style.display = 'none';
-    response.data.forEach((item) => {
+    response.data.expenses.forEach((item) => {
         addRow(item);
     })
+
+    lastPage = response.data.lastPage;
+    value = selected.value;
+    showPages(response.data);
 
     icon.style.display = 'block';
 })
@@ -137,4 +148,51 @@ function addRow(item) {
     // Append the row to the table body
     tableBody.appendChild(newRow);
 
+}
+
+async function updateTableData(e) {
+    tableBody.innerHTML = ``;
+    let page_no = e.target.textContent;
+    let queryParam;
+
+    if (selected === month) queryParam = `?month=${value}&page_no=${page_no}`
+    else queryParam = `?date=${value}&page_no=${page_no}`;
+    const response = await axios.get(`http://localhost:3000/reports/${queryParam}`, { headers: { 'Authorization': token } });
+    if (response.data.expenses.length === 0) {
+        img.style.display = 'block';
+        icon.style.display = 'none';
+        return;
+    }
+
+    img.style.display = 'none';
+    response.data.expenses.forEach((item) => {
+        addRow(item);
+    })
+
+    showPages(response.data);
+
+    icon.style.display = 'block';
+
+}
+function showPages(data) {
+    const pages = document.querySelector('.page-numbers');
+    pages.innerHTML = ``;
+    const createButton = (pageNumber) => {
+        const button = document.createElement('button');
+        button.textContent = pageNumber;
+        button.classList.add('page-number');
+
+        if (pageNumber == data.current) button.classList.add('active');
+
+        button.addEventListener('click', updateTableData);
+        pages.appendChild(button);
+    }
+
+    if (data.current > 1) createButton(1);
+    if (data.current > 2) createButton(data.current - 1);
+
+    createButton(data.current);
+
+    if (data.current < lastPage - 1) createButton(data.current + 1);
+    if (lastPage > data.current) createButton(lastPage);
 }
